28adec686208473531b1dd70321de15966cf4c20
Glen Graham <0xjozeth@gmail.com>
2023-08-23 18:01:18 -0400

    create new controller for CRUD operations, add 'Add Tag' functionality

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        
+        [HttpGet] //explicitly declare the get method as http
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
