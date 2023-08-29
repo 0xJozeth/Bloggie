be404ad19ae0b0b6b74c9f5112bd11c6162edd82
Glen Graham <0xjozeth@gmail.com>
2023-08-28 20:44:25 -0400

    add model binding to read form data | capture data inside of controller

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
-        
         [HttpGet] //explicitly declare the get method as http
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+            
+            return View("Add");
         }
     }
 }
