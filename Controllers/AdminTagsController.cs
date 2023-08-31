87968445f5e60e9c519c17b816652fd6df6a2dfa
Glen Graham <0xjozeth@gmail.com>
2023-08-31 19:26:12 -0400

    add delete funtionality

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -101,5 +101,24 @@ namespace Bloggie.Web.Controllers
             return RedirectToAction("Edit", new { id = editTagRequest.Id });
         }
 
+        [HttpPost]
+
+        public IActionResult Delete(EditTagRequest editTagRequest) 
+        {
+            var tag = bloggieDbContext.Tags.Find(editTagRequest.Id);
+
+            if (tag != null) 
+            {
+                bloggieDbContext.Tags.Remove(tag);
+                bloggieDbContext.SaveChanges();
+
+                // Show a success notification 
+                return RedirectToAction("List");
+            }
+
+            // Show an error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id});
+        }
+
     }
 }
